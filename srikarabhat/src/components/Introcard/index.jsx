33b89739a69194e5f0b98a996915ea02fd4d4917
Srikara S B <srikara.sb@robosoftin.com>
2021-06-25 22:15:32 +0530

    Instro screen animation added

--- srikarabhat/src/components/Introcard/index.jsx
+++ srikarabhat/src/components/Introcard/index.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import classes from "./Introcard.module.scss";
 
 export default function Introcard(props) {
   const { welcomeText, name, downloadLink } = props;
   return (
     <div className={classes.introCardContainer}>
-      Hii Srikara Here.. (Portfolio is under progress)
+      <div className={classes.textContainer}>
+      <div> Hii.. </div> 
+      <div className={classes.name2}>
+        <div className={classes.name}>Srikara</div>{`  Here..,`}
+      </div>
+    
+      </div>
+        <div className={classes.typeWriterText}>
+        <Typewriter 
+          onInit={
+            (typeWriter) => {
+              typeWriter.typeString("React native and React JS developer").pauseFor(2000).start()
+            }
+          }
+        />
+      </div>
     </div>
   );
 }
--- srikarabhat/src/containers/Intro/index.jsx
+++ srikarabhat/src/containers/Intro/index.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import Animate from 'react-smooth';
+import Particles from 'react-particles-js';
 import Images from "../../assets/images/index";
 import Introcard from "../../components/Introcard";
+import particles from "../../assets/particles";
 import classes from "./Intro.module.scss";
 
 export default function Intro() {
   return (
-    <div className={classes.introContainer}>
-      <Introcard />
-      <img className={classes.profilePic} src={Images.profilePic} alt={" "} />
-    </div>
+    <Animate to="1" from="0" attributeName="opacity" className={classes.introContainer}>
+      <Particles
+        params={particles}
+        height="100vh"
+        width="100vw"
+      />
+      <div className={classes.introContainer}>
+          <Introcard/>
+      </div>  
+    </Animate>
   );
 }
